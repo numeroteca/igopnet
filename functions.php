d23513c4f3e150154e3b4a9243d168d7d4f02729
numeroteca <pablo@basurama.org>
2014-12-02 13:16:02 +0100

    Adds Organization Custom post typw

--- functions.php
+++ functions.php
@@ -1,4 +1,34 @@
 <?php
+// Custom post types
+add_action( 'init', 'create_post_type', 0 );
+
+function create_post_type() {
+//Register Organization custom post type
+register_post_type( 'organization', array( // global meetings
+	'labels' => array(
+		'name' => __( 'Organizations' ),
+		'singular_name' => __( 'Organization' ),
+		'add_new_item' => __( 'Add Organization +' ),
+		'edit' => __( 'Edit' ),
+		'edit_item' => __( 'Edit this Organization' ),
+		'new_item' => __( 'New Organization' ),
+		'view' => __( 'View Organization' ),
+		'view_item' => __( 'View Organization' ),
+		'search_items' => __( 'Search Organization' ),
+		'not_found' => __( 'We didn\'t found any Organization' ),
+		'not_found_in_trash' => __( 'No Organization in the trash bin' ),
+		'parent' => __( 'Parent Organization' )
+		),
+	'hierarchical' => true,
+	'public' => true,
+	'menu_position' => 5,
+	'supports' => array('title', 'editor','custom-fields','author','comments','revisions','page-attributes','thumbnail','excerpt'),
+	'rewrite' => array('slug'=>'organization','with_front'=>false),
+	'menu_icon' => 'dashicons-groups',
+	)
+);
+}
+
 //Lists the active languages
 function languages_list(){
     $languages = icl_get_languages('skip_missing=0&orderby=code');
