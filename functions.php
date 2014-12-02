a2322ffd5292cc7bfb92e3415354d8e6db868e21
numeroteca <pablo@basurama.org>
2014-12-02 13:43:47 +0100

    Creates taxonomies for organization custom post type

--- functions.php
+++ functions.php
@@ -1,32 +1,54 @@
 <?php
 // Custom post types
 add_action( 'init', 'create_post_type', 0 );
+// Custom Taxonomies
+add_action( 'init', 'build_taxonomies', 0 );
 
+//Creates Custom Post Types
 function create_post_type() {
-//Register Organization custom post type
-register_post_type( 'organization', array( // global meetings
-	'labels' => array(
-		'name' => __( 'Organizations' ),
-		'singular_name' => __( 'Organization' ),
-		'add_new_item' => __( 'Add Organization +' ),
-		'edit' => __( 'Edit' ),
-		'edit_item' => __( 'Edit this Organization' ),
-		'new_item' => __( 'New Organization' ),
-		'view' => __( 'View Organization' ),
-		'view_item' => __( 'View Organization' ),
-		'search_items' => __( 'Search Organization' ),
-		'not_found' => __( 'We didn\'t found any Organization' ),
-		'not_found_in_trash' => __( 'No Organization in the trash bin' ),
-		'parent' => __( 'Parent Organization' )
-		),
-	'hierarchical' => true,
-	'public' => true,
-	'menu_position' => 5,
-	'supports' => array('title', 'editor','custom-fields','author','comments','revisions','page-attributes','thumbnail','excerpt'),
-	'rewrite' => array('slug'=>'organization','with_front'=>false),
-	'menu_icon' => 'dashicons-groups',
-	)
-);
+	//Register Organization custom post type
+	register_post_type( 'organization', array( // global meetings
+		'labels' => array(
+			'name' => __( 'Organizations' ),
+			'singular_name' => __( 'Organization' ),
+			'add_new_item' => __( 'Add Organization +' ),
+			'edit' => __( 'Edit' ),
+			'edit_item' => __( 'Edit this Organization' ),
+			'new_item' => __( 'New Organization' ),
+			'view' => __( 'View Organization' ),
+			'view_item' => __( 'View Organization' ),
+			'search_items' => __( 'Search Organization' ),
+			'not_found' => __( 'We didn\'t found any Organization' ),
+			'not_found_in_trash' => __( 'No Organization in the trash bin' ),
+			'parent' => __( 'Parent Organization' )
+			),
+		'hierarchical' => true,
+		'public' => true,
+		'menu_position' => 5,
+		'supports' => array('title', 'editor','custom-fields','author','comments','revisions','page-attributes','thumbnail','excerpt'),
+		'rewrite' => array('slug'=>'organization','with_front'=>false),
+		'menu_icon' => 'dashicons-groups',
+		)
+	);
+}
+
+//Creates Taxonomies
+function build_taxonomies() {
+	register_taxonomy( 'org-ecosystem', 'organization', array(
+		'label' => __( 'Ecosystem' ),
+		'rewrite' => array( 'slug' => 'org-ecosystem' ) ) );
+	register_taxonomy( 'org-type', 'organization', array(
+		'label' => __( 'Type' ),
+		'rewrite' => array( 'slug' => 'org-type' ) ) );
+	register_taxonomy( 'org-scope', 'organization', array(
+		'label' => __( 'Scope' ),
+		'rewrite' => array( 'slug' => 'org-scope' ) ) );
+	register_taxonomy( 'org-whom', 'organization', array(
+		'label' => __( 'A quién' ),
+		'rewrite' => array( 'slug' => 'org-whom' ) ) );
+	register_taxonomy( 'org-validation', 'organization', array(
+		'label' => __( 'Validation' ),
+		'rewrite' => array( 'slug' => 'org-validation' ) ) );
 }
 
 //Lists the active languages
