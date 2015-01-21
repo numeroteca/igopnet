a379e29bb0c850881428d1022e184f9712001a5c
numeroteca <pablo@basurama.org>
2015-01-21 11:36:51 +0100

    Fits numeric data in organization box

--- content-organization-box.php
+++ content-organization-box.php
@@ -13,21 +13,25 @@
 	$google_page_rank = $last_url_item['google_page_rank'];
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('col-md-4'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('col-md-4 directory-box'); ?>>
 	<div class="panel panel-default">
 		<div class="row panel-body">
 			<div class="col-md-4">
+				<small>Google Page Rank</small>
 				<div class="progress">
-					<div class="progress-bar" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="100" style="width:<?php echo 100*$google_page_rank/9; ?>%;">
-						<?php echo $google_page_rank; ?> GPR
+					<div class="progress-bar" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="100" title="Google Page Rank <?php echo $google_page_rank; ?>" style="width:<?php echo 100*$google_page_rank/9; ?>%;">
+						<?php echo $google_page_rank; ?>
 					</div>
 				</div>
+				
 				<small>
-					Alexa: <?php echo number_format($last_url_item['alexa_page_rank'], 0, ',', '.'); ?> <br/>
 					<?php
+					if ($last_url_item['alexa_page_rank'] != 0) {
+						echo "Alexa Page Rank:<br/>". number_format($last_url_item['alexa_page_rank'], 0, ',', '.'). "<br/>";
+					}
 					if (!empty($twitter_info)) { //if twitter in time info is not empty
 						if ($twitter_info[0]['followers'] != '') { //if the number of followers is available
-							echo "Twitter: ". number_format($twitter_info[0]['followers'], 0, ',', '.');
+							echo "Twitter Followers:<br/>". number_format($twitter_info[0]['followers'], 0, ',', '.');
 						}
 					} ?>
 				</small>
@@ -50,9 +54,9 @@
 				echo display_tax_link_with_ecosystem($post_id, 'org-type')
 				?><br/>
 				<?php
-				echo !($twitter_account=='') ? "<a href='https://twitter.com/".$twitter_account. "'><img src='".get_stylesheet_directory_uri()."/img/twitter_logo.png' alt='Twitter'></a> " : "" ;
-				echo !($facebook_site=='') ? "<a href='https://facebook.com/".$facebook_site. "'><img src='".get_stylesheet_directory_uri()."/img/facebook_logo.png' alt='Facebook'></a > " : "";
-				echo !($youtube_account=='') ? "<a href='https://youtube.com/".$youtube_account."'><img src='".get_stylesheet_directory_uri()."/img/youtube_logo.png' alt='YouTube'></a>" : "";
+				echo !($twitter_account=='') ? "<a href='https://twitter.com/".$twitter_account. "' title='Twitter'><img src='".get_stylesheet_directory_uri()."/img/twitter_logo.png' alt='Twitter'></a> " : "" ;
+				echo !($facebook_site=='') ? "<a href='https://facebook.com/".$facebook_site. "' title='Facebook'><img src='".get_stylesheet_directory_uri()."/img/facebook_logo.png' alt='Facebook'></a > " : "";
+				echo !($youtube_account=='') ? "<a href='https://youtube.com/".$youtube_account."' title='Youtube'><img src='".get_stylesheet_directory_uri()."/img/youtube_logo.png' alt='YouTube'></a>" : "";
 				?>
 			</div>
 		</div>
