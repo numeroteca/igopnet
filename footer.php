96e5a03f07bfa7361beca70b56567446efde0d83
numeroteca <pablo@basurama.org>
2015-06-08 23:45:59 +0200

    Adds igopnet.cc at the beginning of the footer. Removes Wordpress credits.

--- footer.php
+++ footer.php
@@ -10,7 +10,8 @@
  */
 ?>
 
-	</div><!-- #main -->
+	</div>
+	<!-- #main -->
 
 	<footer id="colophon" role="contentinfo">
 
@@ -19,17 +20,14 @@
 				 * your footer with three columns of widgets.
 				 */
 				if ( ! is_404() )
-					get_sidebar( 'footer' );
+					//get_sidebar( 'footer' );
 			?>
 
-			<div id="site-generator"> <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/es/">Creative Commons Attribution-NonCommercial-ShareAlike</a> |
-				<?php do_action( 'twentyeleven_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyeleven' ); ?>" rel="generator"><?php printf( __( 'Powered by %s', 'twentyeleven' ), 'WordPress' ); ?></a> | Credits: <a href="http://montera34.com">Montera34</a> | RSS: <a href="http://igopnet.cc/?feed=rss2">Entries</a> + <a href="http://igopnet.cc/?feed=comments-rss2">Comments</a> | Contact: igopnet(at)lists.contrast.org
+			<div id="site-generator"><a href="http://igopnet.cc">IGOPnet.cc</a> | <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/es/">Creative Commons Attribution-NonCommercial-ShareAlike</a> | Web credits: <a href="http://montera34.com">Montera34</a> | RSS: <a href="http://igopnet.cc/?feed=rss2">Entries</a> + <a href="http://igopnet.cc/?feed=comments-rss2">Comments</a> | Contact: igopnet(at)lists.contrast.org
 			</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-
 </body>
 </html>
